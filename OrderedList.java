4549a9bb3597cdc76f8f558992156e181c7a8562
ozan-k <ozan.kah@gmail.com>
2023-03-26 21:53:26 +0200

    exercise3

---
+++ OrderedList.java
@@ -0,0 +1,32 @@
+class OrderedList<T extends Comparable<T>> {
+   List<T> list = new ArrayList<>();
+
+   public void add(T element){
+      for (int i = 0; i < list.size(); i++) {
+         T value = list.get(i);
+
+         // if element is smaller than value
+         if (element.compareTo(value) <= 0) {
+            List<T> smallerValues = list.subList(0, i);
+            List<T> higherValues = list.subList(i, list.size());
+
+            list = new ArrayList<>();
+            list.addAll(smallerValues);
+            list.add(element);
+            list.addAll(higherValues);
+            return;
+         }
+      }
+
+      list.add(element);
+   }
+
+   public void remove(T element){
+      list.remove(element);
+   }
+
+   @Override
+   public String toString() {
+      return list.toString();
+   }
+}
